dded0240aebf90d33468a2f82c200189ef01d619
noelia ovella <126622854+noelia-o@users.noreply.github.com>
2023-02-28 22:06:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let php = document.getElementById("php");
+crearBarra(php);
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
+let contadores = [-1,-1,-1,-1,-1,-1];
+let entro = false;
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalWordpress = setInterval(function(){
+            pintarBarra(wordpress, 15, 2, intervalWordpress);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 13, 3, intervalPhotoshop);
+        },100);
+        const intervalPhp = setInterval(function(){
+            pintarBarra(php, 15, 4, intervalPhp);
+        },100);
+        const intervalIlustrator = setInterval(function(){
+            pintarBarra(ilustrator, 11, 5, intervalIlustrator);
+        },100);
+    }
+}
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#940253";
+    }else{
+        clearInterval(interval)
+    }
+}
+window.onscroll = function(){
+    efectoHabilidades();
+}
